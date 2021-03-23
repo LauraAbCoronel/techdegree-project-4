f7fbb3eaa008d14fd998123e25df8c0dd77ea84c
LauraAbCoronel <68669949+LauraAbCoronel@users.noreply.github.com>
2021-03-22 23:45:03 -0400

    added comments & implemented remove life

--- js/Game.js
+++ js/Game.js
@@ -14,6 +14,9 @@ class Game {
         this.activePhrase = null;
     }
 
+    /**
+     * Starts the game off by hiding the overlay screen and getting a random pharase and setting it to the activephrase property
+     */
     startGame() {
         // hides the startgame overlay
         document.querySelector('#overlay').style.display = "none";
@@ -21,17 +24,27 @@ class Game {
         this.activePhrase.addPhraseToDisplay();
     }
 
+    /**
+     * Pick a random number from 0 - length of phrases array and returns that random phrase in the array
+     * @returns {object} - random object phrase in the phrase array
+     */
     getRandomPhrase() {
         const randomNum = Math.floor(Math.random() * this.phrases.length)
         return this.phrases[randomNum];
     }
     
+    /**
+     * Checks the letter button selected is in the phase and calls respected methods 
+     * @param {element} e - button element that triggered the event listener
+     */
     handleInteraction(e) {
         const button = e.target;
         const letter = button.textContent;
+        // disables the button so the user can no longer click the button
         button.disabled = 'true';
         if (this.activePhrase.checkLetter(letter)) {
             button.className = 'chosen';
+            // the matched letter will appear on the webpage
             this.activePhrase.showMatchedLetter(letter);
             if(this.checkForWin()) {
                 this.gameOver();
@@ -42,7 +55,14 @@ class Game {
         }
     }
 
-    removeLife() {}
+    removeLife() {
+        const life = document.querySelectorAll('[src="images/liveHeart.png"]');
+        life[life.length-1].src = "images/lostHeart.png";
+        this.missed += 1;
+        if(this.missed > 4) {
+            this.gameOver();
+        }
+    }
 
     checkForWin() {}
 
