22d5e8a5a9a2d8151fc31803412b61f431a76634
LauraAbCoronel <68669949+LauraAbCoronel@users.noreply.github.com>
2021-03-22 21:58:08 -0400

    Implements addPhraseToDisplay

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,31 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        // takes in the phrase input and converts the letters to lowercase
+        this.phrase = phrase.toLowerCase();
+    }
+
+    addPhraseToDisplay() {
+        const phraseDiv = document.querySelector('#phrase');
+        const phraseUl = phraseDiv.querySelector('ul');
+        const charArr = this.phrase.split("");
+        charArr.forEach(char => {
+            const li = document.createElement('li');
+            if (/[a-z]/.test(char)) {
+                li.className = `hide letter ${char}`;
+                li.textContent = char;
+            } else {
+                li.className = 'space';
+                li.textContent = char;
+            }
+            phraseUl.appendChild(li);
+        });
+    }
+
+    checkLetter(e) {}
+
+    showMatchedLetter() {}
+}
